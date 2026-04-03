f5302fc713be5afec436ba416b51671897bd19f0
maltush <maltushw@gmail.com>
2026-04-03 10:48:09 +0300

    2 этап

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,19 +4,26 @@ from flask import (
     Flask,
     render_template,
 )
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
 load_dotenv()
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
 
+BASE_DIR = os.path.dirname(__file__)        # если файл в page_analyzer — ok
+template_dir = os.path.join(BASE_DIR, 'templates')
+app = Flask(__name__, template_folder=template_dir)
+
+print('HUI', template_dir)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+print("kakashka")
+
+@app.route('/')
+def index():
+    print("piska")
+    return render_template('index.html')
+
+@app.route('/urls_get')
+def urls_get():
+    print("piska + 1")
+    return "pisechka"
 
 if __name__ == '__main__':
     app.run(debug=True)
