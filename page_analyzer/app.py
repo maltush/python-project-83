a7014280aa3a8eac8315837b4fec1f376c5d98a9
maltush <maltushw@gmail.com>
2026-04-28 13:33:57 +0300

    fix2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -11,8 +11,8 @@ from flask import (
     render_template,
     request,
     url_for,
+    get_flashed_messages,
 )
-
 from .parser import check_data
 from .url_repository import UrlRepository
 from .url_validator import normalize_url, validate_url
@@ -31,7 +31,7 @@ print('DB=', DATABASE_URL)
 
 @app.route('/')
 def index():
-    return flask.render_template(
+    return render_template(
         'index.html',
     )
 
@@ -41,7 +41,7 @@ def urls_get():
     conn = psycopg2.connect(DATABASE_URL)
     repo = UrlRepository(conn)
     urls = repo.get_content() or []
-    return flask.render_template(
+    return render_template(
         'urls.html',
          urls=urls
     )
@@ -51,10 +51,10 @@ def urls_get():
 def url_show(id):
     conn = psycopg2.connect(DATABASE_URL)
     repo = UrlRepository(conn)
-    messages = flask.get_flashed_messages(with_categories=True)
+    messages = get_flashed_messages(with_categories=True)
     url = repo.find(id)
     checked_urls = repo.find_checks(id)
-    return flask.render_template(
+    return render_template(
         'show.html',
         url=url,
         checked_urls=checked_urls,
@@ -66,24 +66,24 @@ def url_show(id):
 def url_post():
     conn = psycopg2.connect(DATABASE_URL)
     repo = UrlRepository(conn)
-    url = flask.request.form.to_dict()
+    url = request.form.to_dict()
     errors = validate_url(url['url'])
     
     if errors:
-        flask.flash(errors["url"], 'error')
-        return flask.render_template(
+        flash(errors["url"], 'error')
+        return render_template(
             'index.html',
         ), 422
 
     normalized_url = normalize_url(url['url'])
     url_info = repo.find_by_name(normalized_url)
     if url_info is not None:
-        flask.flash('Страница уже существует', 'error')
-        return flask.redirect(flask.url_for('url_show', id=url_info), code=302)
+        flash('Страница уже существует', 'error')
+        return redirect(url_for('url_show', id=url_info), code=302)
 
     url_id = repo.save(normalized_url)
-    flask.flash('Страница успешно добавлена', 'success')
-    return flask.redirect(flask.url_for('url_show', id=url_id), code=302)
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('url_show', id=url_id), code=302)
 
 
 @app.route('/urls/<int:id>/checks', methods=['POST'])
@@ -100,15 +100,15 @@ def url_check(id):
         response.raise_for_status()
     
     except requests.RequestException:
-        flask.flash('Произошла ошибка при проверке', 'error')
-        return flask.redirect(flask.url_for('url_show', id=id), code=302)
+        flash('Произошла ошибка при проверке', 'error')
+        return redirect(url_for('url_show', id=id), code=302)
 
     status = response.status_code
     data = check_data(response)
     data['status'] = status
     repo.get_checked(data, url_info)
-    flask.flash('Страница успешно проверена', 'success')
-    return flask.redirect(flask.url_for('url_show', id=id), code=302)
+    flash('Страница успешно проверена', 'success')
+    return redirect(url_for('url_show', id=id), code=302)
 
 
 if __name__ == '__main__':
