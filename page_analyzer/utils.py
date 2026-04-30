492e346cd1f6f1f2c16ed05606ec3b915405585e
maltush <maltushw@gmail.com>
2026-04-30 10:52:38 +0300

    Добавила ... и ограничение размера поля

--- page_analyzer/utils.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-
-def truncate(text: Optional[str], n: int = 200) -> Optional[str]:
-    """Truncate text to n characters and append '...' if trimmed."""
-    if text is None:
-        return None
-    return text if len(text) <= n else text[:n] + "..."
